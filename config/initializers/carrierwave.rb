3a1fed401a313168fda1830a692fefc09cc74268
Evgeniy Leontiev <potehin@gmail.com>
2016-07-13 16:55:37 +0400

    try to fix s3 upload 003

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -37,7 +37,7 @@ CarrierWave.configure do |config|
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']                         # required
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                         # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
